3d314114c3da5a41e42fc0d83345b29f66e45ac5
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-05-07 00:54:08 +0530

    added http methods

--- profilesapi/views.py
+++ profilesapi/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+#from rest_framework import serializers
+from profilesapi import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class=serializers.HelloSerializer
+ 
 
     def get(self,request,format=None):
         """Returns a list of APIView features"""
@@ -16,3 +21,28 @@ class HelloApiView(APIView):
         ] 
 
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+
+    def post(self,request):
+        """Create hello msg with name"""
+        serializer=self.serializer_class(data=request.data) 
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')  
+            message=f'Hello {name}'
+            return Response({'message':message}) 
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    def put(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'}) 
+
+    def patch(self,request,pk=None):
+        """Handle partial update of object"""
+        return Response({'method':'PATCH'}) 
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
