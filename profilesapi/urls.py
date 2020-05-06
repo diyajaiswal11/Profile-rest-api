bf43b1f82bf9a354298fb347672f2ac209cc19c8
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-05-07 02:33:17 +0530

    Viewsets added

--- profilesapi/urls.py
+++ profilesapi/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router=DefaultRouter() 
+router.register('helloview',views.HelloViewSet,basename='helloviewset')
+
 
 urlpatterns = [
     path('hello/',views.HelloApiView.as_view()),
-]
+    path('',include(router.urls)),
+ ]
+  
--- profilesapi/views.py
+++ profilesapi/views.py
@@ -4,6 +4,56 @@ from rest_framework.response import Response
 from rest_framework import status
 #from rest_framework import serializers
 from profilesapi import serializers
+from rest_framework import viewsets
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test Api Viewset"""
+
+    serializer_class=serializers.HelloSerializer
+
+    def list(self,request):
+        a_viewset=[
+            'a','b','c',
+        ] 
+        return Response({'msg':'Hello','a_viewset':a_viewset })
+
+
+    def create(self,request):
+        """Create new hello msg"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}' 
+            return Response({'message':message}) 
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+
+        
+    def retrieve(self,request,pk=None):
+        """Handle getting an object by its id"""
+        return Response({'http_method':'GET'}) 
+
+    def update(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'http_method':'PUT'}) 
+
+    def partial_update(self,request,pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method':'PATCH'}) 
+
+    def destroy(self,request,pk=None):
+        """Handle removing an object"""
+        return Response({'http_method':'DELETE'}) 
+
+
+        
+
+
+
+
+
+
 
 class HelloApiView(APIView):
     """Test API View"""
